b71c0c4da93e326ad90f56073a17e5bf99644de2
João <joaopfc2764@gmail.com>
2023-05-05 21:45:39 -0300

    create project

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello World</Text>
+      <Text style={styles.text}>with Expo.io</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#ff0000',
+    fontSize: 28,
+  },
+  text: {
+    color: '#ff0000',
+  }
 });
